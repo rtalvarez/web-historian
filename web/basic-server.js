a8032800f584964a10180fef50f5ccbe2a132039
Hack Reactor Students <robertoatc02@gmail.com>
2014-05-07 15:49:00 -0700

    Server fetches index file

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,15 +1,37 @@
 var http = require("http");
 var handler = require("./request-handler");
 var urlParse = require("url");
+var fs = require("fs");
 
 var port = 8080;
 var ip = "127.0.0.1";
+
+var getIndex = function(request, response, path){
+  fs.readFile(path, function(err, data){
+    if (err) {
+      response.writeHeader(404);
+      console.log(err);
+      response.end("File not found");
+    } else {
+      response.writeHeader(200);
+      response.write(data);
+      response.end();
+    }
+  });
+};
+
 var server = http.createServer(function(request, response){
 
   var path = urlParse.parse(request.url);
   path = path.pathname;
+  console.log(path);
+  if(path === "/"){
+    //return index file
+    getIndex(request, response, "./public/index.html");
+  }else{
+    handler.handleRequest(request, response);
+  }
 });
-  //handler.handleRequest
 
 
 console.log("Listening on http://" + ip + ":" + port);
