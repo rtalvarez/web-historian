77880b3aba88c5b8ed81c501fa3d224bedf88f24
Hack Reactor Students <robertoatc02@gmail.com>
2014-05-07 15:32:54 -0700

    basic server functionality in progress

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,17 @@
 var http = require("http");
 var handler = require("./request-handler");
+var urlParse = require("url");
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+var server = http.createServer(function(request, response){
+
+  var path = urlParse.parse(request.url);
+  path = path.pathname;
+});
+  //handler.handleRequest
+
+
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
