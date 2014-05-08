c9a3d4e758e0812eda88593bfcb0652d81827fa3
Hack Reactor Students <robertoatc02@gmail.com>
2014-05-07 17:04:13 -0700

    Add fetching structure

--- web/basic-server.js
+++ web/basic-server.js
@@ -24,9 +24,8 @@ var server = http.createServer(function(request, response){
 
   var path = urlParse.parse(request.url);
   path = path.pathname;
-  console.log(path);
-  if(path === "/"){
-    //return index file
+  console.log(request.method, path);
+  if(request.method === "GET"){
     getIndex(request, response, "./public/index.html");
   }else{
     handler.handleRequest(request, response);
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,40 @@
+var fs = require("fs");
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var siteList;
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  req.on("data", function(url){
+    fetchTxt(req, res, url);
+    //res.end(archive.paths.list);
+  });
+};
+
+var fetchTxt = function(request, response, url){
+  fs.readFile("../archives/sites.txt", function(err, data){
+    if (err) {
+      response.writeHeader(404);
+      console.log(err);
+      response.end("File not found");
+    } else {
+      response.writeHeader(200);
+      siteList = data.toString().replace(/\n/g," ").split(" ");
+
+      if(searchTxt(url)){
+        //fetch html and respond
+      }else{
+        //send message and go fetch associated html
+      }
+      response.end();
+    }
+  });
+};
+
+var searchTxt = function(url){
+  url = url.slice(url.indexOf("w"));
+  for (var i=0; i<siteList.length; i++){
+    if(siteList[i] === url){return true;}
+  }
+  return false;
 };
