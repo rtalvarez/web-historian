1c75f5bed1f6ef59e34e0d805f6b00870e56cc88
Hack Reactor Students <robertoatc02@gmail.com>
2014-05-07 19:50:22 -0700

    Migrated helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-var httpRequest = require('http-request')
+var httpRequest = require('http-request');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -12,8 +12,9 @@ var httpRequest = require('http-request')
 
 exports.paths = {
   'siteAssets' : path.join(__dirname, '../web/public'),
-  'archivedSites' : path.join(__dirname, '../archives/sites'),
-  'list' : path.join(__dirname, '../archives/sites.txt')
+  'archivedSites' : path.join(__dirname, '../archives/sites/'),
+  'list' : path.join(__dirname, '../archives/sites.txt'),
+  'loadingPage' : path.join(__dirname, './public/loading.html')
 };
 
 // Used for stubbing paths for jasmine tests, do not modify
@@ -26,10 +27,51 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(){
+exports.readListOfUrls = function(request, response, url){
+  fs.readFile(exports.paths.list, function(err, data){
+    if (err) {
+      response.writeHeader(404);
+      response.end('File not found by UrlListReader');
+    } else {
+      response.writeHeader(200);
+      var siteList = data.toString().replace(/\n/g,' ').split(' ');
+
+      if(exports.isUrlInList(siteList, url)){
+        //fetch html and respond
+        fetchSite(request, response, exports.paths.archivedSites+url);
+        console.log("found");
+      }else{
+        //send message and go fetch associated html
+        console.log("not found");
+        // call addTxt
+        fetchSite(request, response, exports.paths.loadingPage);
+        //response.write("We have a bot that is working on it");
+      }
+    }
+  });
 };
 
-exports.isUrlInList = function(){
+var fetchSite = function(request, response, url){
+  console.log(url);
+  fs.readFile(url, function(err, data){
+    if(err){
+      response.writeHeader(404);
+      console.log("fetchSite failed");
+      response.end();
+    }else{
+      console.log("fetched: ", url);
+      response.writeHeader(200, {"Content-Type": "text/html"});
+      response.write(data);
+      response.end();
+    }
+  });
+};
+
+exports.isUrlInList = function(siteList, url){
+  for (var i=0; i<siteList.length; i++){
+    if(siteList[i] === url){return true;}
+  }
+  return false;
 };
 
 exports.addUrlToList = function(url){
--- web/request-handler.js
+++ web/request-handler.js
@@ -8,56 +8,11 @@ exports.handleRequest = function (req, res) {
   req.on("data", function(url){
     url = url.toString();
     url = url.slice(url.indexOf("w"));
-    fetchTxt(req, res, url);
+    archive.readListOfUrls(req, res, url);
     //res.end(archive.paths.list);
   });
 };
 
-var fetchTxt = function(request, response, url){
-  fs.readFile("../archives/sites.txt", function(err, data){
-    if (err) {
-      response.writeHeader(404);
-      response.end("File not found by fetchTxt");
-    } else {
-      response.writeHeader(200);
-      siteList = data.toString().replace(/\n/g," ").split(" ");
-
-      if(searchTxt(url)){
-        //fetch html and respond
-        fetchSite(request, response, "../archives/sites/"+url);
-        console.log("found");
-      }else{
-        //send message and go fetch associated html
-        console.log("not found");
-        // call addTxt
-        fetchSite(request, response, "./public/loading.html");
-        //response.write("We have a bot that is working on it");
-      }
-    }
-  });
-};
-
-var searchTxt = function(url){
-  for (var i=0; i<siteList.length; i++){
-    if(siteList[i] === url){return true;}
-  }
-  return false;
-};
-
-var fetchSite = function(request, response, url){
-  fs.readFile(url, function(err, data){
-    if(err){
-      response.writeHeader(404);
-      console.log("fetchSite failed");
-      response.end();
-    }else{
-      console.log("fetched: ", url);
-      response.writeHeader(200, {"Content-Type": "text/html"});
-      response.write(data);
-      response.end();
-    }
-  });
-};
 
 
 
